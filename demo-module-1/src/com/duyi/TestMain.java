5b3e0f082f302b0217e20d13f03ca301255ad555
kang763501115 <763501115@qq.com>
2023-09-04 10:57:21 +0800

    hello master

---
+++ demo-module-1/src/com/duyi/TestMain.java
@@ -0,0 +1,8 @@
+package com.duyi;
+
+public class TestMain {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello master");
+    }
+}
